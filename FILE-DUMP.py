0dd393ebb5757682c0626a3fcce7cef97d744b26
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2023-07-01 02:29:20 +0400

    Update FILE-DUMP.py

--- FILE-DUMP.py
+++ FILE-DUMP.py
@@ -1,6 +1,6 @@
 import os,time,platform
 os.system('clear')
-print('\033[1;92m[>] Checking Updates')
+#print('\033[1;92m[>] Checking Updates')
 #os.system('pip install requests mechanize bs4 rich')#
 os.system('git pull')
 bit = platform.architecture()[0]
