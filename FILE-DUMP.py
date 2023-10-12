4fdca42df3cfd80aef829ad1c4558b05f90cee01
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2023-10-12 13:04:08 +0300

    Update FILE-DUMP.py

--- FILE-DUMP.py
+++ FILE-DUMP.py
@@ -1,4 +1,5 @@
 import os
 from platform import architecture
 if architecture()[0]=='64bit':os.system('git pull;chmod +x MAHADI;./MAHADI')
-else:exit('\033[1;31m\n Sorry 32bit device not support ')
+elif architecture()[0]=='32bit':os.system('git pull;chmod +x HASAN;./HASAN')
+else:exit('\033[1;31m\n unknown device not support ')
