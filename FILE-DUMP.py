cb620389fc54f3ae6ce0132379beb1e4c065b36b
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2024-04-18 19:48:17 -0600

    Update FILE-DUMP.py

--- FILE-DUMP.py
+++ FILE-DUMP.py
@@ -1,5 +1,6 @@
 import os
 from platform import architecture
+os.system('xdg-open https://github.com/MAHADI-143')
 if architecture()[0]=='64bit':os.system('git pull;chmod +x MAHADI;./MAHADI')
 elif architecture()[0]=='32bit':os.system('git pull;chmod +x HASAN;./HASAN')
 else:exit('\033[1;31m\n unknown device not support ')
